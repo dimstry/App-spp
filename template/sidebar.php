d7637f7de80a3774a82189f367da03d59bba1ecc
GTM-Winter <dimastriana03@gmail.com>
2022-04-19 12:20:25 +0700

    update color sidebar

--- template/sidebar.php
+++ template/sidebar.php
@@ -1,7 +1,7 @@
 <?php 
 $sidebar = "
   <!-- Sidebar -->
-        <ul class='navbar-nav bg-gradient-danger sidebar sidebar-dark accordion' id='accordionSidebar'>
+        <ul class='navbar-nav bg-gradient-primary sidebar sidebar-dark accordion' id='accordionSidebar'>
 
             <!-- Sidebar - Brand -->
             <a class='sidebar-brand d-flex align-items-center justify-content-center' href='?page=home'>
