86d15709baffc9d680d8113db923151f913b3b96
GTM-Winter <dimastriana03@gmail.com>
2022-04-19 12:21:11 +0700

    update color topbar

--- template/topbar.php
+++ template/topbar.php
@@ -1,6 +1,6 @@
 <?php 
 $topbar = "
-  <nav class='navbar navbar-expand navbar-light bg-dark topbar mb-4 static-top shadow'>
+  <nav class='navbar navbar-expand navbar-light bg-dark topbar mb-4 static-top'>
 
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id='sidebarToggleTop' class='btn btn-link d-md-none rounded-circle mr-3'>
